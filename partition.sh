ce4b8b2b510a7eef7c17671990856ccaf1706ce2
osamabel <osamaalpha1997@gmail.com>
2021-11-23 06:24:48 -0500

    modified

--- partition.sh
+++ partition.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 clear
 
-echo "Logic Volume Creation ..."
-source sleep 5
+echo "Create by [ obelkah ]"
+
+echo "#######  Logic Volume Creation ..."
+source sleep 2
 lvcreate.sh
+clear
 
-echo "Formating With ext4 && swap ..."
-sleep 5
+
+echo "#######  Formating With ext4 && swap ..."
+sleep 2
 source filesystem.sh
+clear
 
-echo "mounting ..."
-sleep 5
+
+echo "#######  mounting ..."
+sleep 2
 source mount.sh
+clear
 
-echo "Updatinr fstab ..."
-sleep 5
+echo "#######  Updatinr fstab ..."
+sleep 2
 source up_fstab.sh
+echo "DONE"
--- up_fstab.sh
+++ up_fstab.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+LV_NAMES=(home var srv tmp var--log)
 
+for i in ${LV_NAMES[@]}
+do
+	UUID=blkid | grep ${LV_NAMES[$i]} | cut -d '"' -f 2
+	NAME=blkid | grep $UUID | cut -d ':' -f 1 | cut -c 22-
+	echo $NAME ":" $UUID
+done
 
-
-
-
-vv=(jj jj jj)
-
-mkdir /var/twil
