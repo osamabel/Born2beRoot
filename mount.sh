0cbd8c76a88b1d37d99ca0a0ca5ac07b79a1e1ca
osamabel <osamaalpha1997@gmail.com>
2021-11-23 06:01:19 -0500

    modified mount.sh

--- mount.sh
+++ mount.sh
@@ -4,10 +4,11 @@ lv_names=(home var srv tmp var--log)
 lv_path=/dev/mapper/LVMGroup-
 moint_point=(home var srv tmp var/log)
 
-mkdir -p /var/log
 
 for i in ${!lv_names[@]}
 do
-mount /dev/mapper/LVMGroup-home /home
+	if [ ${lv_names[$i]} = "var--log" ]; then
+		mkdir -p /var/log
+	fi
 	mount ${lv_path}${lv_names[$i]} /${moint_point[$i]}
 done
